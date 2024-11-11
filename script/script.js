556851996cf49172891bab3a6958b19f8cd8767c
lvx222 <146774813+pierre-222@users.noreply.github.com>
2024-11-11 21:16:58 +0100

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,23 +1,23 @@
-        let slideIndex = 0;
-        showSlides();
+let slideIndex = 0;
+showSlides();
 
-        function plusSlides(n) {
-            slideIndex += n;
-            showSlides();
-        }
+function plusSlides(n) {
+    slideIndex += n;
+    showSlides();
+}
 
-        function showSlides() {
-            let slides = document.getElementsByClassName("slide");
-            if (slideIndex >= slides.length) {
-                slideIndex = 0;
-            }
-            if (slideIndex < 0) {
-                slideIndex = slides.length - 1;
-            }
+function showSlides() {
+    let slides = document.getElementsByClassName("slide");
+    if (slideIndex >= slides.length) {
+        slideIndex = 0;
+    }
+    if (slideIndex < 0) {
+        slideIndex = slides.length - 1;
+    }
 
-            for (let i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-            }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
 
-            slides[slideIndex].style.display = "block";
-        }
+    slides[slideIndex].style.display = "block";
+}
