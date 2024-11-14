a91b04c0880b6d9d79ae70f97e990436eeb63cb5
lvx222 <146774813+pierre-222@users.noreply.github.com>
2024-11-14 21:28:07 +0100

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,23 +1,28 @@
-let slideIndex = 0;
-showSlides();
-
-function plusSlides(n) {
-    slideIndex += n;
-    showSlides();
+/// Fonction pour ouvrir l'image en modale
+function openImageModal() {
+    // Afficher le modal
+    document.getElementById('menuImage').style.display = 'flex';
+    // Ajouter la classe no-scroll au body pour empêcher le scroll de la page
+    document.body.classList.add('no-scroll');
+    // Faire défiler l'image en haut de la modale
+    const modalContent = document.querySelector('.image-modal-content');
+    modalContent.scrollTop = 0;  // S'assurer que l'image commence en haut
 }
 
-function showSlides() {
-    let slides = document.getElementsByClassName("slide");
-    if (slideIndex >= slides.length) {
-        slideIndex = 0;
-    }
-    if (slideIndex < 0) {
-        slideIndex = slides.length - 1;
-    }
-
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-
-    slides[slideIndex].style.display = "block";
+// Fonction pour fermer la modale
+function closeImageModal() {
+    // Cacher le modal
+    document.getElementById('menuImage').style.display = 'none';
+    // Retirer la classe no-scroll pour permettre le scroll de la page
+    document.body.classList.remove('no-scroll');
 }
+
+
+// Ouvrir l'image en cliquant sur le lien
+document.querySelector('.open-image-link').addEventListener('click', function(event) {
+    event.preventDefault();
+    openImageModal();
+});
+
+
+
